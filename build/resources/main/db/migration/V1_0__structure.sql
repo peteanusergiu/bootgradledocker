9d92205f363692ccbeb820f4ac11d459b9221f46
sergiu petean <peteanusergiu@gmail.com>
2015-10-15 15:46:06 +0200

    [TechReq#Flyway integration]: adding flyway integration for db initialization

---
+++ build/resources/main/db/migration/V1_0__structure.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customer (
+  id mediumint(8) NOT NULL,
+  firstname varchar(100) DEFAULT NULL,
+  lastname varchar(100) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
---
+++ build/resources/main/db/migration/V1_1__initialize.sql
@@ -0,0 +1,6 @@
+INSERT INTO customer (id, firstname, lastname)
+  VALUES
+    (1, 'firstname1', 'lastname1'),
+    (2, 'firstname2', 'lastname2'),
+    (3, 'firstname3', 'lastname3'),
+    (4, 'firstname4', 'lastname4');
