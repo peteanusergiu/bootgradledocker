6c987a3b86154c47604c4d1d787ec36b762ac70d
sergiu petean <peteanusergiu@gmail.com>
2015-10-20 11:51:54 +0200

    [TechReq#boot profile]: making spring boot active profile configurable

--- src/test/java/org/experiment/RestServiceControllerTest.java
+++ src/test/java/org/experiment/RestServiceControllerTest.java
@@ -6,10 +6,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+//use gradle task test -Dspring.profiles.active=devh2l
 //@ActiveProfiles(EnvironmentUtils.DEV_H2)
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -18,7 +20,12 @@ public class RestServiceControllerTest extends AbstractRestServiceControllerTest
 	
 	@Test
 	public void testSayHello() throws Exception{
-		mvc.perform(get("/hello/world/visitor/sergiu")).andExpect(status().is2xxSuccessful());
+		String profileName = activeProfile.getProfileName();
+		System.out.println(profileName);
+		ResultActions andExpect = mvc.perform(get("/hello/world/visitor/" + profileName)).andExpect(status().is2xxSuccessful());
+		String contentAsString = andExpect.andReturn().getResponse().getContentAsString();
+
+		System.out.println(contentAsString);
 	}
 	
 }
