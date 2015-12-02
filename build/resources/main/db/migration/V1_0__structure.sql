d3f6890d9e097872102870caa3f2c36a8d850032
Sergiu Petean <peteanusergiu@gmail.com>
2015-12-02 12:42:38 +0100

    [QualityReq#cleaninig unneded git files]

--- build/resources/main/db/migration/V1_0__structure.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE test.Customer (
-  id INTEGER PRIMARY KEY,
-  firstname varchar(100),
-  lastname varchar(100)
-);
--- build/resources/main/db/migration/V1_1__initialize.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO test.Customer (id, firstname, lastname)
-  VALUES
-    (1, 'firstname1', 'lastname1'),
-    (2, 'firstname2', 'lastname2'),
-    (3, 'firstname3', 'lastname3'),
-    (4, 'firstname4', 'lastname4');
